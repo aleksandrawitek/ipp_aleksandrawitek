2064f456ce769f7ac888752d101321fbda04d905
Aleksandra Witek <olciawitek@wp.pl>
2021-03-21 16:43:59 +0100

    gui

--- kalendarz/kalendarz/settings.py
+++ kalendarz/kalendarz/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,9 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#ustawienie odpowiedniej strefy czasowej oraz jezyka
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'pl-pl'
+
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -119,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- kalendarz/kalendarzapp/views.py
+++ kalendarz/kalendarzapp/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello World")
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
