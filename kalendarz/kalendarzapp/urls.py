de807612388e0f4d8ebb33718dca0d4c6791263a
Aleksandra Witek <olciawitek@wp.pl>
2021-04-04 10:08:40 +0200

    users

--- kalendarz/kalendarzapp/urls.py
+++ kalendarz/kalendarzapp/urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 
 from . import views
 
-urlpatterns = [path('', views.glowna, name = 'glowna'),]
+urlpatterns = [
+    path('', views.glowna, name = 'glowna'),
+    path('register/', views.register, name = 'register'),
+    ]
--- kalendarz/kalendarzapp/views.py
+++ kalendarz/kalendarzapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-
+from .forms import CreateUserForm
 
 def glowna(request):
     return render(request, 'kalendarzapp/glowna.html', {})
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST' :
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'kalendarzapp/register.html', {'form' :form})
