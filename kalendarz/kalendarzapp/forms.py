14a963e7daef6b1cc13a01a065fbe403a13bba1f
Aleksandra Witek <olciawitek@wp.pl>
2021-04-08 14:07:00 +0200

    users

--- kalendarz/kalendarzapp/forms.py
+++ kalendarz/kalendarzapp/forms.py
@@ -6,4 +6,4 @@ from django.contrib.auth.models import User
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email','password1', 'password2']
+        fields = ['username', 'first_name', 'last_name','email','password1', 'password2']
