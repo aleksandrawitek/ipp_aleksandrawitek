b1662cf6b362b768cd62e5aa206f101ab14b57d9
Aleksandra Witek <olciawitek@wp.pl>
2021-04-10 18:05:40 +0200

    users

--- kalendarz/kalendarzapp/urls.py
+++ kalendarz/kalendarzapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.glowna, name = 'glowna'),
     path('register/', views.register, name = 'register'),
+    path('login/', views.loginPage, name = 'login'),
+    path('logout/', views.logoutPage, name = 'logout')
     ]
--- kalendarz/kalendarzapp/views.py
+++ kalendarz/kalendarzapp/views.py
@@ -1,17 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-from .forms import CreateUserForm
 
+@login_required(login_url='login')
 def glowna(request):
     return render(request, 'kalendarzapp/glowna.html', {})
 
 def register(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('glowna')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST' :
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, "Zarejestrowano użytkownika " + user)
+                return redirect(loginPage)
+        return render(request, 'kalendarzapp/register.html', {'form' :form})
 
-    if request.method == 'POST' :
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-    return render(request, 'kalendarzapp/register.html', {'form' :form})
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('glowna')
+    else:
+        if request.method == 'POST' :
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request,user)
+                    return redirect('glowna')
+                else:
+                    messages.info(request, "Użytkownik lub hasło jest niepoprawne")
+        return render(request, 'kalendarzapp/login.html', {})
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
+
+
