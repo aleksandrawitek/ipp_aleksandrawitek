edd8a560a6c634779a52404326be16e34001fd68
Aleksandra Witek <olciawitek@wp.pl>
2021-05-07 23:54:27 +0200

    addnew

--- kalendarz/kalendarz/settings.py
+++ kalendarz/kalendarz/settings.py
@@ -124,3 +124,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#email
+
+EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'ipp.awitek@gmail.com'
+EMAIL_HOST_PASSWORD = 'Djangowebsite1'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
--- kalendarz/kalendarzapp/views.py
+++ kalendarz/kalendarzapp/views.py
@@ -5,6 +5,8 @@ from .forms import CreateUserForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import *
+from django.core.mail import send_mail
+from kalendarz.settings import EMAIL_HOST_USER
 
 # Create your views here.
 
@@ -20,7 +22,31 @@ def addnew(request):
     meetings = list(Meeting.objects.all())
     blocks = list(Block.objects.all())
     email = request.user.email
-    return render(request, 'kalendarzapp/addnew.html', {'meetings':meetings, 'blocks': blocks, 'email': email})
+    user = request.user.username
+    if request.method == 'POST' :
+        if request.POST.get('day') and request.POST.get('month') and request.POST.get('year') and request.POST.get('hour_start') and request.POST.get('minute_start') and request.POST.get('hour_stop') and request.POST.get('minute_stop'):
+            meeting = Meeting()
+            meeting.day = request.POST.get('day')
+            meeting.month = request.POST.get('month')
+            meeting.year = request.POST.get('year')
+            meeting.user = user
+            meeting.hour_start = request.POST.get('hour_start')
+            meeting.hour_stop = request.POST.get('hour_stop')
+            meeting.minute_start = request.POST.get('minute_start')
+            meeting.minute_stop = request.POST.get('minute_stop')
+            meeting.save()
+            tekst = "Cześć, " +  user + "!" + "\n" + "\n" + "Dodano nowe spotkanie - korepetycje matematyka: \n \n" + "Data: " + str(request.POST.get('day')) + "-" + str(request.POST.get('month')) + "-" + str(request.POST.get('year')) +",\n" + 'Godzina rozpoczęcia: ' + str(request.POST.get('hour_start')) +':'+ str(request.POST.get('minute_start')) + ',\n' + 'Godzina zakończenia: ' + str(request.POST.get('hour_stop')) +":"+ str(request.POST.get('minute_stop')) + ".\n \n" + "W razie pytań proszę się kontaktować pod numerem 123-456-789. \n \n" + "Pozdrawiam, \n Aleksandra Witek"
+            send_mail('Potwierdzenie umówienia wizyty',
+            tekst,
+            EMAIL_HOST_USER, 
+            [str(email)],
+            fail_silently= False)
+            messages.info(request, "Poprawnie zarejestrowano spotkanie")
+            return render(request, 'kalendarzapp/addnew.html', {'meetings':meetings, 'blocks': blocks, 'email': email})
+        else:
+            messages.info(request, "Niepoprawnie uzupełniono dane")
+    else:
+        return render(request, 'kalendarzapp/addnew.html', {'meetings':meetings, 'blocks': blocks, 'email': email})
 
 
 def register(request):
